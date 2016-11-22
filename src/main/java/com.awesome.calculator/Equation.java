af5c7909fac468256b4937c4128c7eff4e19c569
David Nguyen <david.nguyen@netis.be>
2016-11-23 00:00:40 +0100

    fix operator weigth sort

--- src/main/java/com.awesome.calculator/Equation.java
+++ src/main/java/com.awesome.calculator/Equation.java
@@ -7,6 +7,7 @@ import com.google.common.collect.Multimap;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -68,8 +69,7 @@ public class Equation {
     String currentEquationString = equationString;
 
     List<Integer> weights = new ArrayList<>(operatorsByWeight.keySet());
-    Collections.sort(weights);
-    Collections.reverse(weights);
+    Collections.sort(weights, Comparator.<Integer>naturalOrder().reversed());
     for (int weight : weights) {
       currentEquationString = searchAndCompute(currentEquationString, operatorsByWeight.get(weight));
     }
